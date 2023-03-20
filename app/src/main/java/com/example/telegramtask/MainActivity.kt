3ccce7cc83fb505fc2cc2887891bf3b651b691c3
TarekIdrees <tarekdrias321@gmail.com>
2023-03-20 12:59:57 +0200

    initial commit

---
+++ app/src/main/java/com/example/telegramtask/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.telegramtask
+
+import android.graphics.PorterDuff
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import androidx.constraintlayout.motion.widget.MotionLayout
+import com.google.android.material.appbar.AppBarLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
